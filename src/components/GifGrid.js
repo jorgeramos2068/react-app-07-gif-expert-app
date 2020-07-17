1d06b23f624964e13797fb05692c818e4e192ae7
Jorge Ramos <jorgeramos2068@gmail.com>
2020-07-17 14:17:59 -0600

    Implement app to load data from Giphy

---
+++ src/components/GifGrid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import GifGridItem from './GifGridItem';
+import {useFetchGifs} from '../hooks/useFetchGifs';
+
+const GifGrid = ({category}) => {
+  const {data:images, loading} = useFetchGifs(category);
+
+  return (
+    <>
+      <h3>{category}</h3>
+      {loading && <p>Loading</p>}
+      {<div className="card-grid">
+        <ol>
+          {
+            images.map((image) => (
+              <GifGridItem key={image.id} {...image} />
+            ))
+          }
+        </ol>
+      </div>}
+    </>
+  );
+}
+
+export default GifGrid;
